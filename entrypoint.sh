752640dda1b84ebec4e555f940c4cd66231a7555
Andrea Cervesato <andrea.cervesato@kqi.it>
2019-07-31 22:15:12 +0200

    Addedo SIGTERM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-/bin/dht_exporter.py --sensor ${SENSOR} -p ${PULL} --gpio ${GPIO} --room ${ROOM}
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGTERM
+echo "Starting exporter on port 8001 with options: --sensor ${SENSOR} -p ${PULL} --gpio ${GPIO} --room ${ROOM}"
+/bin/dht_exporter.py --sensor ${SENSOR} -p ${PULL} --gpio ${GPIO} --room ${ROOM} &
+child=$! 
+wait "$child"
